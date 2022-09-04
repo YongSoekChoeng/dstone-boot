d4734bff609264438aa87709144ad1468b0c96cc
YongSoekChoeng <jysn007@gmail.com>
2022-09-04 16:18:02 +0900

    .

--- bin/startApp.bat
+++ bin/startApp.bat
@@ -1,4 +1,6 @@
 
 set JAVA_OPTS=-Djava.ext.dirs=C:/DEV/JDK/1.8/jre/lib/ext;C:/Windows/Sun/Java/lib/ext;D:/AppHome/dstone-boot/extLib
-start /B javaw %JAVA_OPTS% -jar ../target/dstone-boot-0.0.1-SNAPSHOT.war net.dstone.DstoneBootApplication
+set CONF_FILE=-Dspring.config.location=file:../conf/application.yml
+set JAR_FILE=../target/dstone-boot-0.0.1-SNAPSHOT.war
+start /B javaw %JAVA_OPTS% %CONF_FILE% -jar %JAR_FILE% net.dstone.DstoneBootApplication
 
