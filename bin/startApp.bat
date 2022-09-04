87daa158ed798f9e7ccdec5a0c23ba628d049709
YongSoekChoeng <jysn007@gmail.com>
2022-09-04 16:57:25 +0900

    .

--- bin/startApp.bat
+++ bin/startApp.bat
@@ -1,5 +1,5 @@
 
-set JAVA_OPTS=-Djava.ext.dirs=C:/DEV/JDK/1.8/jre/lib/ext;C:/Windows/Sun/Java/lib/ext;D:/AppHome/dstone-boot/extLib
+set JAVA_OPTS=-Xms512m -Xmx1024m -Djava.ext.dirs=C:/DEV/JDK/1.8/jre/lib/ext;C:/Windows/Sun/Java/lib/ext;D:/AppHome/dstone-boot/extLib
 set CONF_FILE=-Dspring.config.location=file:../conf/application.yml
 set JAR_FILE=../target/dstone-boot-0.0.1-SNAPSHOT.war
 start /B javaw %JAVA_OPTS% %CONF_FILE% -jar %JAR_FILE% net.dstone.DstoneBootApplication
--- bin/startApp.sh
+++ bin/startApp.sh
@@ -1,5 +1,5 @@
 
-JAVA_OPTS:="-Djava.ext.dirs=/DEV/JDK/1.8/jre/lib/ext:/Sun/Java/lib/ext:/AppHome/dstone-boot/extLib"
+JAVA_OPTS:="-Xms512m -Xmx1024m -Djava.ext.dirs=/DEV/JDK/1.8/jre/lib/ext:/Sun/Java/lib/ext:/AppHome/dstone-boot/extLib"
 CONF_FILE:="-Dspring.config.location=file:../conf/application.yml"
 JAR_FILE="../target/dstone-boot-0.0.1-SNAPSHOT.war"
 javaw ${JAVA_OPTS} ${CONF_FILE} -jar ${JAR_FILE} net.dstone.DstoneBootApplication
