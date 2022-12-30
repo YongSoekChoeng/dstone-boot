a2fb8a027bc9fcc10087286962bfe2da8fd1a470
YongSoekChoeng <jysn007@gmail.com>
2022-12-31 03:20:13 +0900

    .

--- bin/startApp.sh
+++ bin/startApp.sh
@@ -5,6 +5,6 @@ JAR_FILE="../target/dstone-boot-0.0.1-SNAPSHOT.war"
 APP_CONF_FILE="--spring.config.location=file:../conf/application.yml"
 LOG_CONF_FILE="--logging.config=file:../conf/log4j2.xml"
 
-nohup java ${JAVA_OPTS} -jar ${JAR_FILE} net.dstone.DstoneBootApplication ${APP_CONF_FILE} ${LOG_CONF_FILE} > /dev/null &
+nohup java ${JAVA_OPTS} -jar ${JAR_FILE} net.dstone.DstoneBootApplication ${APP_CONF_FILE} ${LOG_CONF_FILE} > /dev/null 2>&1
 # java ${JAVA_OPTS} -jar ${JAR_FILE} net.dstone.DstoneBootApplication ${APP_CONF_FILE} ${LOG_CONF_FILE}
 
