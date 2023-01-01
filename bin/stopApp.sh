7aa955d112fb06dfa56bc2984227df84476690f6
YongSoekChoeng <jysn007@gmail.com>
2023-01-01 19:26:19 +0900

    .

--- bin/stopApp.sh
+++ bin/stopApp.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
 
-PID=`cat application.pid`
-echo ${PID}
-
-kill -15 ${PID}
-rm application.pid
+FILE="application.pid"
+if [ -e $FILE ]; then
+	PID=`cat application.pid`
+	echo ${PID}
+	
+	kill -15 ${PID}
+	rm application.pid
+fi
