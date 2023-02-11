5b9e1d0ee33fd057fc5e1a8fdf34e6d0093ce686
YongSoekChoeng <jysn007@gmail.com>
2023-02-11 16:48:45 +0900

    .

--- src/main/java/net/dstone/sample/webstress/WebStressQueueItem.java
+++ src/main/java/net/dstone/sample/webstress/WebStressQueueItem.java
@@ -7,17 +7,19 @@ public class WebStressQueueItem extends QueueItem {
 
 	@Override
 	public void doTheJob() {
+		
 		String filePath = this.getProperty("filePath");
 		String fileName = this.getProperty("fileName");
 		
 		try {
 			//net.dstone.sample.dept.DeptService svc = (net.dstone.sample.dept.DeptService)net.dstone.common.utils.BeanUtil.getSpringBean(net.dstone.sample.dept.DeptService.class);
-			Thread.sleep(500);
+			
 
 			String fileConts = net.dstone.common.utils.DateUtil.getToDate("yyyyMMdd-HH:mm:ss") + "에 파일내용.";
 			net.dstone.common.utils.FileUtil.writeFile(filePath, fileName, fileConts);
-			
+			Thread.sleep(500);
 			net.dstone.common.utils.FileUtil.deleteFile(filePath + "/" + fileName);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
