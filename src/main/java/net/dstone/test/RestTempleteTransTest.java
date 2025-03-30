8d0a9f8e2a9c0b0d186c9c53dbaf19ce874ffaaa
YongSoekChoeng <jysn007@gmail.com>
2025-03-30 19:48:35 +0900

    .

--- src/main/java/net/dstone/test/RestTempleteTransTest.java
+++ src/main/java/net/dstone/test/RestTempleteTransTest.java
@@ -1,6 +1,7 @@
 package net.dstone.test;
 
 import java.nio.charset.Charset;
+import java.util.Arrays;
 import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
@@ -15,6 +16,9 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import net.dstone.common.utils.DataSet;
 
@@ -40,27 +44,43 @@ public class RestTempleteTransTest extends net.dstone.common.core.BaseObject {
 				String hKey = reqHeaderKeys.nextElement();
 				httpHeaders.add(hKey, request.getHeader(hKey));
 			}
+			info("request.getContentType()====>>>["+request.getContentType()+"]");
 			httpHeaders.setContentType(MediaType.valueOf(request.getContentType()));
 
 			/* 2. Body 및 HttpEntity 생성 */
-			// Json 방식
-			if( httpHeaders.getContentType().equals(MediaType.APPLICATION_JSON) ||  httpHeaders.getContentType().equals(MediaType.APPLICATION_JSON_UTF8) ) {
-				DataSet reqDs = new DataSet();
-				reqDs.setDatum("TEST", "과일목록");
-				requestEntity = new HttpEntity<String>(reqDs.toJson(), httpHeaders);
-			// Form	방식
-			}else {
-				MultiValueMap<String, String> reqParam = new LinkedMultiValueMap<>();
-				reqParam.add("TEST", "과일목록");
-				requestEntity = new HttpEntity<Map>(reqParam, httpHeaders);
+			// METHOD - POST, PUT 방식
+			if( request.getMethod().toUpperCase().equals("POST") || request.getMethod().toUpperCase().equals("PUT") ) {
+				// Json 방식
+				if( httpHeaders.getContentType().equals(MediaType.APPLICATION_JSON) ||  httpHeaders.getContentType().equals(MediaType.APPLICATION_JSON_UTF8) ) {
+					String body = IOUtils.toString(request.getInputStream(), Charset.forName(request.getCharacterEncoding()));
+info( this.getClass().getName() + ".callTest ::: body 라인 53===>>>" + body );
+					requestEntity = new HttpEntity<String>(body, httpHeaders);					
+				// Form	방식
+				}else {
+					MultiValueMap<String, Object> reqParam = new LinkedMultiValueMap<>();
+					java.util.Enumeration<String> keys = request.getParameterNames();
+					while( keys.hasMoreElements() ) {
+						String key = keys.nextElement();
+						if( request.getParameterValues(key).length > 1) {
+							String[] valArr = request.getParameterValues(key);
+							for( String val : valArr ) {
+								reqParam.add(key, val);
+info( this.getClass().getName() + ".callTest ::: body 라인 68===>>>["+key+"]" + val );	
+							}
+						}else {
+							reqParam.add(key, request.getParameter(key));
+info( this.getClass().getName() + ".callTest ::: body 라인 72===>>>["+key+"]" + request.getParameter(key) );	
+						}
+					}
+					requestEntity = new HttpEntity<MultiValueMap>(reqParam, httpHeaders);		
+				}
+			// METHOD - 나머지 방식
+			}else{
+				requestEntity = new HttpEntity<String>("", httpHeaders);
 			}
 			
-	    	info( this.getClass().getName() + ".callTest =================================>>> line 62 requestEntity.getBody():" + requestEntity.getBody() );
-
-			/* 3. RestTemplate 생성 */
+			/* 3. RestTemplate 호출 */
 			restTemplate = net.dstone.common.utils.RestFulUtil.getInstance().getRestTemplate();
-
-			/* 4. RestTemplate 호출 */
 			responseEntity = restTemplate.exchange(
 				"http://localhost:7081/test/rest/callProxy.do",
 	             HttpMethod.valueOf(request.getMethod()),
@@ -68,7 +88,7 @@ public class RestTempleteTransTest extends net.dstone.common.core.BaseObject {
 	             String.class
 	        );
 
-			System.out.println( "callTest ::: responseEntity===>>>" + responseEntity );
+	    	info( this.getClass().getName() + ".callTest ::: responseEntity===>>>" + responseEntity );
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -81,10 +101,11 @@ public class RestTempleteTransTest extends net.dstone.common.core.BaseObject {
     @RequestMapping(value = "/callProxy.do") 
     public ResponseEntity callProxy(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws Exception {
 
-    	info( this.getClass().getName() + ".callProxyPost has been called !!!" );
+    	info( this.getClass().getName() + ".callProxy has been called !!!" );
 
-    	RestTemplate restTemplate = net.dstone.common.utils.RestFulUtil.getInstance().getRestTemplate();
-    	ResponseEntity<String> responseEntity = null;
+    	RestTemplate restTemplate = null;
+    	HttpEntity<?> requestEntity = null;
+    	ResponseEntity<?> responseEntity = null;
        
         try {
         	
@@ -95,16 +116,50 @@ public class RestTempleteTransTest extends net.dstone.common.core.BaseObject {
 				String hKey = reqHeaderKeys.nextElement();
 				httpHeaders.add(hKey, request.getHeader(hKey));
 			}
+
+			/* 2. Body 및 HttpEntity 생성 */
+			// METHOD - POST, PUT 방식
+			if( request.getMethod().toUpperCase().equals("POST") || request.getMethod().toUpperCase().equals("PUT") ) {
+				// Json 방식
+				if( httpHeaders.getContentType().equals(MediaType.APPLICATION_JSON) ||  httpHeaders.getContentType().equals(MediaType.APPLICATION_JSON_UTF8) ) {
+					String body = IOUtils.toString(request.getInputStream(), Charset.forName(request.getCharacterEncoding()));
+info( this.getClass().getName() + ".callProxy ::: body 라인 126===>>>" + body );
+					requestEntity = new HttpEntity<String>(body, httpHeaders);					
+				// Form	방식
+				}else {
+					MultiValueMap<String, Object> reqParam = new LinkedMultiValueMap<>();
+					java.util.Enumeration<String> keys = request.getParameterNames();
+					while( keys.hasMoreElements() ) {
+						String key = keys.nextElement();
+						if( request.getParameterValues(key).length > 1) {
+							String[] valArr = request.getParameterValues(key);
+							for( String val : valArr ) {
+								reqParam.add(key, val);
+info( this.getClass().getName() + ".callProxy ::: body 라인 138===>>>["+key+"]" + val );	
+							}
+						}else {
+							reqParam.add(key, request.getParameter(key));
+info( this.getClass().getName() + ".callProxy ::: body 라인 142===>>>["+key+"]" + request.getParameter(key) );	
+						}
+					}
+					requestEntity = new HttpEntity<MultiValueMap>(reqParam, httpHeaders);		
+				}
+			// METHOD - 나머지 방식
+			}else{
+				requestEntity = new HttpEntity<String>("", httpHeaders);
+			}
 			
-        	String body = IOUtils.toString(request.getInputStream(), Charset.forName(request.getCharacterEncoding()));
-			
+			/* 3. RestTemplate 호출 */
+	    	restTemplate = net.dstone.common.utils.RestFulUtil.getInstance().getRestTemplate();
 			responseEntity = restTemplate.exchange(
-				"http://localhost:7081/test/rest/callMain.do",
-                HttpMethod.valueOf(request.getMethod()),
-                new HttpEntity<>(body, httpHeaders),
-                String.class
+				"http://localhost:7081/test/rest/callMain.do"
+				, HttpMethod.valueOf(request.getMethod())
+				, requestEntity
+				, String.class
             );
 
+	    	info( this.getClass().getName() + ".callProxy ::: responseEntity===>>>" + responseEntity );
+			
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -122,7 +177,37 @@ public class RestTempleteTransTest extends net.dstone.common.core.BaseObject {
 		try {
 
 			/* 1. Body 생성 */
-        	String body = IOUtils.toString(request.getInputStream(), Charset.forName(request.getCharacterEncoding()));
+			if( request.getMethod().toUpperCase().equals("POST") || request.getMethod().toUpperCase().equals("PUT") ) {
+				// Json 방식
+				if( request.getContentType().equals(MediaType.APPLICATION_JSON.getType()) ||  request.getContentType().equals(MediaType.APPLICATION_JSON_UTF8.getType()) ) {
+					String body = IOUtils.toString(request.getInputStream(), Charset.forName(request.getCharacterEncoding()));
+info( this.getClass().getName() + ".callMain ::: body 라인 154===>>>" + body.toString() );					
+				// Form	방식
+				}else {
+					java.util.Enumeration<String> keys = request.getParameterNames();
+					while( keys.hasMoreElements() ) {
+						String key = keys.nextElement();
+						String val = request.getParameter(key);
+						if( request.getParameterValues(key)  != null && request.getParameterValues(key).length > 1) {
+info( this.getClass().getName() + ".callMain ::: body 라인 163===>>>["+key+"]" + java.util.Arrays.toString(request.getParameterValues(key)) );	
+						}else{
+info( this.getClass().getName() + ".callMain ::: body 라인 165===>>>["+key+"]" + request.getParameter(key) );	
+						}
+					}
+				}
+			// METHOD - 나머지 방식
+			}else{
+				Map<String, String[]> reqMap = request.getParameterMap();
+				java.util.Iterator<String> keys = reqMap.keySet().iterator();
+				while( keys.hasNext() ) {
+					String key = keys.next();
+info( this.getClass().getName() + ".callMain ::: body 라인 170===>>>" + request.getParameter(key) );		
+				}
+			}
+			
+	    	info( this.getClass().getName() + ".callMain ::: request===>>>" + request );
+
+			/* 1. Body 생성 */
         	DataSet resDs = new DataSet();
         	resDs.setDatum("NAME", "과일목록");
 			DataSet rowDs = resDs.addDataSet("FRUIT_LIST");
