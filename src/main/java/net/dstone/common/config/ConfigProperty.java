cc4de8573e9ec462863b1b5598cdfa8a7df89692
YongSoekChoeng <jysn007@gmail.com>
2025-04-27 15:46:34 +0900

    .

--- src/main/java/net/dstone/common/config/ConfigProperty.java
+++ src/main/java/net/dstone/common/config/ConfigProperty.java
@@ -19,7 +19,7 @@ public class ConfigProperty {
 	private static final LogUtil logger = new LogUtil(ConfigProperty.class);
 
 	/**
-	 * env는 기본적으로 application.yml의 프로퍼티정보를 로딩한다. @PropertySource 가 세팅되어 있으면 해당 프로퍼티의 정보도 시스템프로퍼티로 로딩한다. 
+	 * Environment는 기본적으로 application.yml의 프로퍼티정보를 로딩한다. @PropertySource 가 세팅되어 있으면 해당 프로퍼티의 정보도 시스템프로퍼티로 로딩한다. 
 	 * 이 경우 application.yml에서 시스템프로퍼티를 ${프로퍼티} 형식으로 사용할 수 있다.
 	 */
 	@Autowired 
