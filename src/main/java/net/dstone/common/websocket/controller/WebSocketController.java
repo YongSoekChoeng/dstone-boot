549bae9d95fe10690e90c5a8aea6332b4dac880f
YongSeokChoeng <jysn007@gmail.com>
2025-06-30 07:53:58 +0900

    .

--- src/main/java/net/dstone/common/websocket/controller/WebSocketController.java
+++ src/main/java/net/dstone/common/websocket/controller/WebSocketController.java
@@ -8,23 +8,17 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.dstone.common.biz.BaseController;
 import net.dstone.common.utils.LogUtil;
 
 @Configuration
 @ConditionalOnProperty(name = "spring.websocket.enabled", havingValue = "true")
 @RestController
-public class WebSocketController {
-
-	private static LogUtil logger = new LogUtil(WebSocketController.class);
+public class WebSocketController extends BaseController {
 
     @Autowired
 	private SimpMessagingTemplate simpMessagingTemplate;       // 특정 사용자에게 메시지를 보내는데 사용되는 STOMP을 이용한 템플릿입니다.
-//
-//    @Autowired
-//    public WebSocketController(SimpMessagingTemplate template) {
-//        this.simpMessagingTemplate = template; 
-//    }
-//	
+
     @MessageMapping("/message")
     public void sendMessage(@RequestBody String payload) {
     	simpMessagingTemplate.convertAndSend("/sub/message", payload);       // 구독중인 모든 사용자에게 메시지를 전달합니다.
