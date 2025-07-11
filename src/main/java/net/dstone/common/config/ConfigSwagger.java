ab33d8e7e5cfc33816f67095f1d42ddb5f5819d2
YongSeokChoeng <jysn007@gmail.com>
2025-07-11 15:32:49 +0900

    .

--- src/main/java/net/dstone/common/config/ConfigSwagger.java
+++ src/main/java/net/dstone/common/config/ConfigSwagger.java
@@ -25,30 +25,27 @@ public class ConfigSwagger {
             .select()
             // 스캔 대상 클래스
             .apis(requestHandler -> {
-            	
+            	/*** 스캔 대상 클래스가 추가될 때 세팅될 설정 시작 ***/
             	boolean isValid = false;
-            	
                 Class<?> declaringClass = requestHandler.declaringClass();
             	boolean isRootPackage = declaringClass.getName().startsWith("net.dstone.sample.swagger");
             	boolean isControllerClass = declaringClass.isAnnotationPresent(Controller.class);
             	boolean isRestControllerClass = declaringClass.isAnnotationPresent(RestController.class);
-            	// @Api 어노테이션 존재여부
             	boolean isApiClass = declaringClass.isAnnotationPresent(Api.class);
-            	
             	isValid = (isRootPackage && (isControllerClass || isRestControllerClass) && isApiClass);
-            	//System.out.println("declaringClass.getName()===========>>>>" + declaringClass.getName() + ", isValid:" + isValid);
+            	/*** 스캔 대상 클래스가 추가될 때 세팅될 설정 끝 ***/
                 return isValid;
             })
-            // 스캔 대상 Path
+            // 스캔 대상 경로
             .paths(path -> {
-            	/*** 모듈이 추가될 때 세팅될 설정 시작 ***/
+            	/*** 스캔 대상경로가 추가될 때 세팅될 설정 시작 ***/
             	// 조건 - 샘플1
                 boolean condition1 = PathSelectors.ant("/restapi/sample/**").apply(path);
             	// 조건 - 샘플2
                 boolean condition2 = PathSelectors.ant("/restapi/sample2/**").apply(path);
                 // 조건 조합 리턴
                 return ( condition1 || condition2 );
-            	/*** 모듈이 추가될 때 세팅될 설정 끝 ***/
+            	/*** 스캔 대상경로가 추가될 때 세팅될 설정 끝 ***/
             })
             .build()
             .apiInfo(apiInfo());
