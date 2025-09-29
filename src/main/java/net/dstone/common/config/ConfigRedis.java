9664837e7c9929dac1dc39940c0c065460ff5ccf
YongSoekChoeng <jysn007@gmail.com>
2025-09-29 16:00:22 +0900

    .

--- src/main/java/net/dstone/common/config/ConfigRedis.java
+++ src/main/java/net/dstone/common/config/ConfigRedis.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
