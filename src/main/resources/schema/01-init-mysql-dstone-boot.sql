e74022367e8f2668241810317f1228c5ae8762cb
YongSoekChoeng <jysn007@gmail.com>
2025-12-14 13:14:32 +0900

    .

--- src/main/resources/schema/01-init-mysql-dstone-boot.sql
+++ src/main/resources/schema/01-init-mysql-dstone-boot.sql
@@ -1,17 +1,17 @@
--- 서버용 Sample 데이터베이스 생성
+-- Sample 데이터베이스[Application용] 생성
 CREATE DATABASE IF NOT EXISTS sampleDB CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
--- 서버용 Sample 사용자 생성 및 권한 부여
+-- Sample 데이터베이스[Application용] 사용자 생성 및 권한 부여
 CREATE USER IF NOT EXISTS 'sampleuser'@'%' IDENTIFIED BY 'sampleuser';
 GRANT ALL PRIVILEGES ON sampleDB.* TO 'sampleuser'@'%';
 
 -- 권한 적용
 FLUSH PRIVILEGES;
 
--- 서버용 Analyze 데이터베이스 생성
+-- Analyze 데이터베이스[Application용] 생성
 CREATE DATABASE IF NOT EXISTS analyzeDB CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
--- 서버용 Analyze 사용자 생성 및 권한 부여
+-- Analyze 데이터베이스[Application용] 사용자 생성 및 권한 부여
 CREATE USER IF NOT EXISTS 'analyzeuser'@'%' IDENTIFIED BY 'analyzeuser';
 GRANT ALL PRIVILEGES ON analyzeDB.* TO 'analyzeuser'@'%';
 
