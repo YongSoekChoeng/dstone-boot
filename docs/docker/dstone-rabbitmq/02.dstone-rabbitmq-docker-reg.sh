567f8a55c9c777dca37c0d7af995cbcc4dd6de3c
YongSoekChoeng <jysn007@gmail.com>
2025-12-15 10:17:34 +0900

    .

--- docs/docker/dstone-rabbitmq/02.dstone-rabbitmq-docker-reg.sh
+++ docs/docker/dstone-rabbitmq/02.dstone-rabbitmq-docker-reg.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 
-docker commit dstone-rabbitmq dstone-rabbitmq:v0.8
-docker tag dstone-rabbitmq:v0.8 jysn007/dstone-rabbitmq:v0.8
+# Docker Hub에 로그인
 docker login -u jysn007 -p 'db2admin!@'
-docker push jysn007/dstone-rabbitmq:v0.8
+# 컨테이너 → 이미지 커밋(버전태깅포함)
+docker commit dstone-rabbitmq dstone-rabbitmq:v1.0
+# 최종버전태그 추가
+docker tag dstone-rabbitmq:v1.0 jysn007/dstone-rabbitmq:latest
+# 두개태그(특정했던버전 + 최종버전) 푸시
+docker push jysn007/dstone-rabbitmq:v1.0
+docker push jysn007/dstone-rabbitmq:latest
