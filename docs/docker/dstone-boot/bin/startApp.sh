dc881ac29b31fd70695beb90dfa98338b677ae9b
YongSoekChoeng <jysn007@gmail.com>
2025-12-16 10:49:48 +0900

    .

--- docs/docker/dstone-boot/bin/startApp.sh
+++ docs/docker/dstone-boot/bin/startApp.sh
@@ -3,5 +3,9 @@
 JAVA_OPTS="-Xms512m -Xmx1024m"
 JAR_FILE="../target/dstone-boot.war"
 
-nohup java ${JAVA_OPTS} -jar ${JAR_FILE} net.dstone.DstoneBootApplication
-# nohup java ${JAVA_OPTS} -jar ${JAR_FILE} net.dstone.DstoneBootApplication > /dev/null 2>&1 &
+APP_CONF_FILE="--spring.config.location=file:../conf/application.yml"
+LOG_CONF_FILE="--logging.config=file:../conf/log4j2.xml"
+
+nohup java ${JAVA_OPTS} -jar ${JAR_FILE} net.dstone.DstoneBootApplication ${APP_CONF_FILE} ${LOG_CONF_FILE} > /dev/null 2>&1 &
+# java ${JAVA_OPTS} -jar ${JAR_FILE} net.dstone.DstoneBootApplication ${APP_CONF_FILE} ${LOG_CONF_FILE}
+
--- src/main/resources/schema/02-create-table-mysql-dstone-boot.sql
+++ src/main/resources/schema/02-create-table-mysql-dstone-boot.sql
@@ -54,6 +54,14 @@ CREATE TABLE IF NOT EXISTS SAMPLE_ROLE_PROG (
   PRIMARY KEY  (ROLE_ID, PROG_ID)
 ) ;
 
+CREATE TABLE IF NOT EXISTS SAMPLE_TEST (
+  TEST_ID VARCHAR(30) NOT NULL, 
+  TEST_NAME VARCHAR(200), 
+  FLAG_YN VARCHAR(1), 
+  INPUT_DT DATE NOT NULL,  
+  PRIMARY KEY  (TEST_ID)
+) ;
+
 /**********************************************
 Analyze 데이터베이스[Application용] 테이블
 **********************************************/
